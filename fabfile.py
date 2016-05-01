32cc7d24921eb552ff8e2f7dba635ff2433ca97c
riccardoNovaglia <riccardo.novaglia@gmail.com>
2016-05-01 16:12:12 +0100

    Changed source to .

--- fabfile.py
+++ fabfile.py
@@ -6,13 +6,13 @@ def setup_env(name='local_env'):
     print '--- Creating virtual environment at ./{}'.format(name)
     local('virtualenv {}'.format(name))
 
-    with prefix('source {}/bin/activate'.format(name)):
+    with prefix('. {}/bin/activate'.format(name)):
         print '--- Virtual environment created, installing dependencies from requirements.txt'
         local('pip install -r requirements.txt')
 
     print '-----------------------------------------\n\n' \
           'Virtual environment created at ./{0}\n' \
-          'To activate use "source {0}/bin/activate"'.format(name)
+          'To activate use ". {0}/bin/activate"'.format(name)
 
 
 def unit_test(args=''):
