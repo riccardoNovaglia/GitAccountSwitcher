465d75b0439b7b0071ba6888976bff6334bbac98
riccardoNovaglia <riccardo.novaglia@gmail.com>
2016-05-29 09:14:34 +0100

    Remove files helper, not needed anymore

--- gitSwitch/helpers/Files.py
+++
@@ -1,8 +0,0 @@
-def write_to_file(file_path, content):
-    with open(file_path, 'w+') as file_stream:
-        file_stream.write(content)
-
-
-def append_to_file(file_path, content):
-    with open(file_path, 'a') as file_stream:
-        file_stream.write(content)
