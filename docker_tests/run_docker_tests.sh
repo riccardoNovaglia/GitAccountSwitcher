c2b78cf378d0a4673af85826ff5a5a9d939cdc5e
riccardoNovaglia <riccardo.novaglia@gmail.com>
2016-08-07 12:27:35 +0100

    Update script to run with docker to remove containers after run to avoid taking up space

--- docker_tests/run_docker_tests.sh
+++ docker_tests/run_docker_tests.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-eval $(docker-machine env --shell bash)
 
 docker build -t functionaltest .
 
 if [ $# -eq 1 ];
 then
-    echo "docker run -it functionaltest docker_tests:only='$1'"
-    docker run -it functionaltest docker_tests:only=\'$1\'
+    docker run --rm functionaltest docker_tests:only=\'$1\'
 else
-    echo else
-    docker run -it functionaltest
+    docker run --rm functionaltest
 fi
